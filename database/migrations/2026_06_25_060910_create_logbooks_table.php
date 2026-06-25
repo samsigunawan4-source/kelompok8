18f67296238291df7bc601f70af68346b5fc1ea1
Samsi Gunawan Iwo <email_github_kamu@gmail.com>
2026-06-25 14:18:38 +0800

    feat: mengatur relasi eloquent untuk tabel magang dan logbook

---
+++ database/migrations/2026_06_25_060910_create_logbooks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logbooks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('internship_id')->constrained()->onDelete('cascade');
+            $table->date('date');
+            $table->text('activity');
+            $table->string('documentation')->nullable(); // Untuk upload gambar
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logbooks');
+    }
+};
